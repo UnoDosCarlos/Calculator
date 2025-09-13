5e0395e6e2a441bae42a28e43f5d8e7c16a40293
UnoDosCarlos <the.immortal.carlos@gmail.com>
2025-09-13 12:35:59 -0400

    Add query selectors for all buttons
    Added query selectors for all the different buttons on the calculator that are going
    to be used in calculations.

---
+++ javascript.js
@@ -0,0 +1,52 @@
+
+const screen = document.querySelector("#screen")
+const clear = document.querySelector('#clear');
+const backspace = document.querySelector('#backspace');
+const divide = document.querySelector('#divide');
+const one = document.querySelector('#1');
+const two = document.querySelector('#2');
+const three = document.querySelector('#3');
+const multiply = document.querySelector('#multiply');
+const four = document.querySelector('#4');
+const five = document.querySelector('#5');
+const six = document.querySelector('#6');
+const subtract = document.querySelector('#subract');
+const seven = document.querySelector('#7');
+const eight = document.querySelector('#8');
+const nine = document.querySelector('#9');
+const add = document.querySelector('#add');
+const zero = document.querySelector('#0');
+const decimal = document.querySelector('#decimal');
+
+
+
+function getNum()
+
+
+
+
+
+
+
+function addNumbers()
+
+
+
+
+
+
+function subNumbers()
+
+
+
+
+
+
+function mulNumbers()
+
+
+
+
+
+
+function divNumbers()
