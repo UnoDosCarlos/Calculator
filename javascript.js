4bda6fb227a4830fad261db1ccd3c8cc4d266739
UnoDosCarlos <the.immortal.carlos@gmail.com>
2025-09-15 14:51:23 -0400

    Adjust html buttons;change color for operators
    Changed the html button formats to prepare for functionality;Adusted color and added
    effects to button hover and press to improve user interface functionality.

--- javascript.js
+++ javascript.js
@@ -1,52 +1,20 @@
 
-const screen = document.querySelector("#screen")
+const screen = document.getElementById("#screen")
 const clear = document.querySelector('#clear');
 const backspace = document.querySelector('#backspace');
 const divide = document.querySelector('#divide');
-const one = document.querySelector('#1');
-const two = document.querySelector('#2');
-const three = document.querySelector('#3');
 const multiply = document.querySelector('#multiply');
-const four = document.querySelector('#4');
-const five = document.querySelector('#5');
-const six = document.querySelector('#6');
 const subtract = document.querySelector('#subract');
-const seven = document.querySelector('#7');
-const eight = document.querySelector('#8');
-const nine = document.querySelector('#9');
 const add = document.querySelector('#add');
-const zero = document.querySelector('#0');
 const decimal = document.querySelector('#decimal');
+const allButtons = document.querySelectorAll('button');
 
 
+for(let btn of allButtons) {
+    btn.addEventListener('click',() => {
+        let currentValue = Number(btn.value);
+        console.log(currentValue);
+    })
 
-function getNum()
+}
 
-
-
-
-
-
-
-function addNumbers()
-
-
-
-
-
-
-function subNumbers()
-
-
-
-
-
-
-function mulNumbers()
-
-
-
-
-
-
-function divNumbers()
