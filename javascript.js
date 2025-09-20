a785f719da7975ed918fd58984475455482dc420
UnoDosCarlos <the.immortal.carlos@gmail.com>
2025-09-20 13:07:34 -0400

    Add number display to calculator
    Added functions to allow button presses to display on screen.

--- javascript.js
+++ javascript.js
@@ -1,27 +1,68 @@
-
-const screen = document.getElementById("screen")
+const screen = document.querySelector(".screen")
 const clear = document.querySelector('#clear');
 const backspace = document.querySelector('#backspace');
-const divide = document.querySelector('#divide');
-const multiply = document.querySelector('#multiply');
-const subtract = document.querySelector('#subract');
-const add = document.querySelector('#add');
-const decimal = document.querySelector('#decimal');
-const allButtons = document.querySelectorAll('button');
+const numBtns = document.querySelectorAll('.number-button');
+const opBtns = document.querySelectorAll('.operand-button');
+const equals = document.querySelector('.equals');
+const prevNum = document.querySelector('.previous-number');
+const currNum = document.querySelector('.current-number');
+
+class Calculator {
+        constructor(prevNum, currNum) {
+            this.prevNum = prevNum;
+            this.currNum = currNum;
+            clearScreen()
+
+        }
+
+}
+
+const calculator = new Calculator(prevNum, currNum);
+
+numBtns.forEach(button => {
+
+    button.addEventListener('click', () => {
+
+        showOnScreen(button.textContent)
+        updateScreen();
+    })
+
+})
 
 
+function showOnScreen(number) {
+    if(number === '.' && this.currNum.includes('.')) { 
+        return
+    };
+this.currNum = this.currNum.toString() + number.toString();
 
-function showOnScreen(input) {
-screen.value += input;
 
 }
 
 function clearScreen() {
-screen.value = '';
+this.currNum = '';
+this.prevNum = '';
+operation = undefined;
 
 }
 
 function removeNumber() {
-screen.value = screen.value.slice(0, -1);
+
+}
+
+function chooseOperator(operation) {
+
+
+}
+
+
+function calculate() {
+
+}
+
+function updateScreen() {
+    currNum.textContent = this.currNum;
+
+
 
 }
