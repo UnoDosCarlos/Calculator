3ef3a7b6ceec49d50475a05fe5bb395ca6ccbf71
UnoDosCarlos <the.immortal.carlos@gmail.com>
2025-09-23 18:18:39 -0400

    Adjust display to show operands and calculations
    Made adjustments to the display to properly show user operands
    used, created logic for the buttons, and adjusted display to allow very large numbers.

--- javascript.js
+++ javascript.js
@@ -39,13 +39,25 @@ opBtns.forEach(button => {
 
     
     })
-
-
 })
 
 
+equals.addEventListener('click', () => {
+    calculate();
+    updateScreen();
+});
 
+backspace.addEventListener('click', () => {
+    removeNumber();
+    updateScreen();
 
+});
+
+clear.addEventListener('click', () => {
+    clearScreen();
+    updateScreen();
+
+});
 
 function showOnScreen(number) {
     if(number === '.' && this.currNum.includes('.')) { 
@@ -64,6 +76,7 @@ operation = undefined;
 }
 
 function removeNumber() {
+    this.currNum = this.currNum.toString().slice(0 ,-1);
 
 }
 
@@ -79,21 +92,79 @@ this.operation = operation;
 this.prevNum = this.currNum;
 this.currNum = '';
 
-
 }
 
 
 function calculate() {
 
+let calculation;
+const prev = parseFloat(this.prevNum);
+const curr = parseFloat(this.currNum);
+if (isNaN(prev) || isNaN(curr)) {
+    return;
+}
 
+switch (this.operation) {
 
+    case '+':
+        calculation = prev + curr
+        break;
+
+    case '-':
+        calculation = prev - curr;
+        break;
+
+    case '*':
+        calculation = prev * curr;
+        break;
+
+    case '/':
+        calculation = prev / curr;
+        break;
+
+    case 'x²':
+        calculation = Math.pow(prev, curr);
+        break;
+
+    default:
+        return;
 
 }
 
+this.currNum = calculation;
+this.operation = undefined;
+this.prevNum = '';
+
+
+};
+
+function getDisplayNumber(number) {
+    const stringNum = number.toString();
+    const intDigits = parseFloat(stringNum.split('.') [0])
+    const decimalDigits = stringNum.split('.')[1]
+    let intDisplay;
+    if(isNaN(intDigits)) {
+        intDisplay = '';
+    } else {
+        intDisplay = intDigits.toLocaleString('en', {
+            maximumFractionDigits: 0})
+    
+    }
+    if(decimalDigits != null) {
+        return `${intDisplay}.${decimalDigits}`
+    } else {
+        return intDisplay;
+    }
+}
+
+
 function updateScreen() {
 
-    currNum.textContent = this.currNum;
-    prevNum.textContent = this.prevNum;
-    
+    currNum.textContent = this.getDisplayNumber(this.currNum);
+    if(this.operation != null) {
+    prevNum.textContent = `${this.getDisplayNumber(this.prevNum)} ${this.operation}`;
+    } else {
+        prevNum.textContent = '';
+    }
 
 }
